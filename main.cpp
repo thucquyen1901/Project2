15d41bd7160568b615ace5858d9487f164105e92
pleasehangry <nguyenquanghoang12345@gmail.com>
2021-10-17 22:34:11 +0700

    Update

--- main.cpp
+++ main.cpp
@@ -7,8 +7,12 @@ using namespace std;
 
 int main(){;
     NhanVien n1( "1", "Quang Hoang", "3-5-2002", "Hue", "0918636253", "192033798", "NhanVienBanVe", "Ban ve");
+    NhanVien n2( "1", "Quang Hoang", "3-5-2002", "Hue", "0918636253", "192033798", "NhanVienBanVe", "Ban ve");
+    NhanVien n3( "1", "Quang Hoang", "3-5-2002", "Hue", "0918636253", "192033798", "NhanVienBanVe", "Ban ve");
     QuanLyNhanVien M1;
     M1.Add_NV(n1);
+    M1.Add_NV(n2);
+    M1.Add_NV(n3);
     M1.Show();
     return 0;
 }
